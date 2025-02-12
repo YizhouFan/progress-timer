6a627656a951c365e571e2b254306ace4925fa99
Yizhou Fan <leafjensen14@gmail.com>
2025-02-12 15:52:25 +0900

    Add pause and unpause features

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ with open("data/example_project.json") as f:
 
 
 def update_start_ts(ts):
-    DATA["sessions"].append({"start_ts": ts})
+    DATA["sessions"].append({"start_ts": ts, "latest_active_ts": ts})
     DATA["status"] = "started"
     with open("data/example_project.json", "w") as f:
         json.dump(DATA, f, indent=4)
@@ -23,32 +23,69 @@ def update_stop_ts(ts):
     session = DATA["sessions"][-1]
     session["stop_ts"] = ts
     session["duration"] = ts - session["start_ts"]
-    session["progress"] = []
-    for tracker in CONFIG["progress_trackers"]:
-        progress = int(input(f"Enter the progress of {tracker['name']}: "))
-        session["progress"].append(
-            {"tracker_name": tracker["name"], "progress": progress}
-        )
+    if DATA["status"] == "unpaused":
+        net_duration_list = session.get("net_duration", [])
+        net_duration_list.append(ts - session["latest_active_ts"])
+        session["net_duration"] = net_duration_list
+    session["progress"] = input_progress()
+    session["final_net_duration"] = sum(
+        session.get("net_duration", [session["duration"]])
+    )
     DATA["sessions"][-1] = session
     DATA["status"] = "stopped"
     with open("data/example_project.json", "w") as f:
         json.dump(DATA, f, indent=4)
 
 
+def input_progress():
+    result = []
+    for tracker in CONFIG["progress_trackers"]:
+        progress = int(input(f"Enter the progress of {tracker['name']}: "))
+        result.append({"tracker_name": tracker["name"], "progress": progress})
+    return result
+
+
 def show_session_summary():
     latest_session = DATA["sessions"][-1]
-    human_readable_time = human_readable_time_string(latest_session["duration"])
-    total_duration = sum([s["duration"] for s in DATA["sessions"]])
+    human_readable_time = human_readable_time_string(
+        latest_session["final_net_duration"]
+    )
+    total_net_duration = sum([s["final_net_duration"] for s in DATA["sessions"]])
     print(
-        f"Good Job! You worked on {CONFIG['project_name']} for {human_readable_time} in this session. You've been working on it for {human_readable_time_string(total_duration)}."
+        f"Good Job! You worked on {CONFIG['project_name']} for {human_readable_time} in this session. You've been working on it for {human_readable_time_string(total_net_duration)}."
     )
     for i, tracker in enumerate(CONFIG["progress_trackers"]):
         total_progress = sum([s["progress"][i]["progress"] for s in DATA["sessions"]])
-        velocity = total_progress / total_duration
+        velocity = total_progress / total_net_duration
         forecasted_duration = tracker["total"] / velocity
         print(
             f"Progress for {tracker['name']} is now {total_progress}(+{latest_session['progress'][i]['progress']})/{tracker['total']} or {total_progress/tracker['total']*100:.2f}%(+{latest_session['progress'][i]['progress']/tracker['total']*100:.2f}%). The progress will be 100% in {human_readable_time_string(forecasted_duration)}."
         )
+
+
+def update_pause_ts(ts):
+    session = DATA["sessions"][-1]
+    pause_ts_list = session.get("pause_ts", [])
+    pause_ts_list.append(ts)
+    session["pause_ts"] = pause_ts_list
+    net_duration_list = session.get("net_duration", [])
+    net_duration_list.append(ts - session["latest_active_ts"])
+    session["net_duration"] = net_duration_list
+    DATA["status"] = "paused"
+    with open("data/example_project.json", "w") as f:
+        json.dump(DATA, f, indent=4)
+
+
+def update_unpause_ts(ts):
+    session = DATA["sessions"][-1]
+    unpause_ts_list = session.get("unpause_ts", [])
+    unpause_ts_list.append(ts)
+    session["unpause_ts"] = unpause_ts_list
+    assert len(session["unpause_ts"]) == len(session["pause_ts"])
+    session["latest_active_ts"] = ts
+    DATA["status"] = "unpaused"
+    with open("data/example_project.json", "w") as f:
+        json.dump(DATA, f, indent=4)
 
 
 def start():
@@ -64,7 +101,7 @@ def start():
 
 
 def stop():
-    if DATA["status"] == "started":
+    if DATA["status"] in ["started", "paused", "unpaused"]:
         ts = datetime.now().timestamp()
         update_stop_ts(ts)
         show_session_summary()
@@ -77,10 +114,28 @@ def stop():
         raise NotImplementedError(f"Unknown status {DATA['status']} detected!")
 
 
+def pause():
+    if DATA["status"] in ["started", "unpaused"]:
+        ts = datetime.now().timestamp()
+        update_pause_ts(ts)
+    else:
+        raise NotImplementedError(f"Unknown status {DATA['status']} detected!")
+
+
+def unpause():
+    if DATA["status"] == "paused":
+        ts = datetime.now().timestamp()
+        update_unpause_ts(ts)
+    else:
+        raise NotImplementedError(f"Unknown status {DATA['status']} detected!")
+
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "mode", type=str, help="Run mode. Now supports start, stop, and stats"
+        "mode",
+        type=str,
+        help="Run mode. Now supports start, stop, pause, unpause, stats.",
     )
     args = parser.parse_args()
 
@@ -88,8 +143,12 @@ def main():
         start()
     elif args.mode == "stop":
         stop()
+    elif args.mode == "pause":
+        pause()
+    elif args.mode == "unpause":
+        unpause()
     elif args.mode == "stats":
-        pass
+        show_session_summary()
     else:
         raise NotImplementedError(f"Run mode {args.mode} is not supported!")
 
