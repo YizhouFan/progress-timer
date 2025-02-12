be65343bb30f9f4c6a30ac912bb4abecc6f91233
Yizhou Fan <leafjensen14@gmail.com>
2025-02-12 14:29:54 +0900

    Fixed a bug for human readable time string converter

--- utils.py
+++ utils.py
@@ -11,10 +11,10 @@ def human_readable_time_string(seconds):
     if seconds >= 60:
         minutes = seconds // 60
         postfix = "minutes" if minutes > 1 else "minute"
-        result += f" {minutes:.0f} minutes"
+        result += f" {minutes:.0f} {postfix}"
     seconds -= minutes * 60
     if seconds >= 1:
         postfix = "seconds" if seconds > 1 else "seconds"
-        result += f" {seconds:.0f} seconds"
+        result += f" {seconds:.0f} {postfix}"
     result = result.strip()
     return result
